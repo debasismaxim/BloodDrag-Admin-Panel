30632378f6df2d9d422902fe564835c45214e899
Rakesh Acharya <rakesh>
2022-03-14 22:08:03 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,9 @@ export const environment = {
   production: true,
   apiUrl: "https://api.blooddrag.com/api",//"http://ec2-3-135-246-141.us-east-2.compute.amazonaws.com/api",
   baseUrl: "https://api.blooddrag.com",//"http://ec2-3-135-246-141.us-east-2.compute.amazonaws.com/api/static",
-  currentUrl: "http://betaadmin.blooddrag.com/" //"http://ec2-13-58-107-244.us-east-2.compute.amazonaws.com/"
+  currentUrl: "http://betaadmin.blooddrag.com/", //"http://ec2-13-58-107-244.us-east-2.compute.amazonaws.com/",
+  recaptcha : {
+    siteKey : "6LfmtNceAAAAALZKBCZxLhn817tGOwaKN_tazMUG",
+    secretKey : "6LfmtNceAAAAALfD5bMXl8ApsMmGxyLCfjm7mdNk"
+  }
 };
