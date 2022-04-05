e258e38e56668e5286650f32cd52d2bd2ea4d979
Rakesh <rakesh.acharya3.imit@gmail.com>
2022-04-05 21:20:06 +0530

    new api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: "https://api.blooddrag.com/api",//"http://ec2-3-135-246-141.us-east-2.compute.amazonaws.com/api",
-  baseUrl: "https://api.blooddrag.com",//"http://ec2-3-135-246-141.us-east-2.compute.amazonaws.com/api/static",
-  currentUrl: "http://betaadmin.blooddrag.com/", //"http://ec2-13-58-107-244.us-east-2.compute.amazonaws.com/",
+  apiUrl: "http://65.2.10.217:5001/api",//"http://ec2-3-135-246-141.us-east-2.compute.amazonaws.com/api",
+  baseUrl: "http://65.2.10.217:5001/api",//"http://ec2-3-135-246-141.us-east-2.compute.amazonaws.com/api/static",
+  currentUrl: "http://65.2.10.217:4300/", //"http://ec2-13-58-107-244.us-east-2.compute.amazonaws.com/",
   recaptcha : {
     siteKey : "6LcThukeAAAAAA1TZW48vXEBYk-MOic8gLRCLiy0",
     secretKey : "6LcThukeAAAAAB8yLM2jPIy8IftNi_6f_Y_R5q8F"
