b4ba7106e5ff189671555819f28e86716a62428f
Rakesh <rakesh.acharya3.imit@gmail.com>
2022-04-05 21:25:18 +0530

    new api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: "http://65.2.10.217:5001/api",//"http://ec2-3-135-246-141.us-east-2.compute.amazonaws.com/api",
   baseUrl: "http://65.2.10.217:5001/api",//"http://ec2-3-135-246-141.us-east-2.compute.amazonaws.com/api/static",
-  currentUrl: "http://65.2.10.217:4300/", //"http://ec2-13-58-107-244.us-east-2.compute.amazonaws.com/",
+  currentUrl: "http://65.2.10.217:8000/", //"http://ec2-13-58-107-244.us-east-2.compute.amazonaws.com/",
   recaptcha : {
     siteKey : "6LcThukeAAAAAA1TZW48vXEBYk-MOic8gLRCLiy0",
     secretKey : "6LcThukeAAAAAB8yLM2jPIy8IftNi_6f_Y_R5q8F"
